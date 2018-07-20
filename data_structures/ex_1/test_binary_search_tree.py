9475a39de6ea62495b0f72c8bae4408a2cba4344
William VanDolah <william.vandolah@gmail.com>
2018-07-20 10:38:59 -0500

    depth first done

--- data_structures/ex_1/test_binary_search_tree.py
+++ data_structures/ex_1/test_binary_search_tree.py
@@ -14,6 +14,7 @@ class BinarySearchTreeTests(unittest.TestCase):
     self.bst.insert(7)
     self.bst.insert(9)
     self.bst.depth_first_for_each(cb)
+    
 
     self.assertEqual(arr, [5, 2, 3, 7, 9])
 
